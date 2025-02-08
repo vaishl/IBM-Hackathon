3b479ec365219a882a7229ab9569a4d75e178d20
Derp707 <134079988+Derp707@users.noreply.github.com>
2025-02-08 15:35:54 +0000

    Update SavingAppScript.js

--- SavingAppScript.js
+++ SavingAppScript.js
@@ -1,1 +1,14 @@
+var button = document.getElementById("saveButton");
 
+function buttonHover()
+{
+  button.style.backgroundColor = "#DBDBDB";
+}
+
+function buttonReset()
+{
+  button.style.backgroundColor = "#F0F0F0";
+}
+
+button.addEventListener('mouseover', buttonHover);
+button.addEventListener('mouseout', buttonReset);
